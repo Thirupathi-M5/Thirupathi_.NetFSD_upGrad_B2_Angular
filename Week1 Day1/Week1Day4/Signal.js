59b3f8359cea5a3f3ee1daf0e21f93f3a0ba711a
Kumbar Thirupathi <155380490+Thirupathi-M5@users.noreply.github.com>
2026-02-20 09:23:32 +0530

    Add files via upload

---
+++ Week1 Day1/Week1Day4/Signal.js
@@ -0,0 +1,17 @@
+let signal="red"
+switch(signal){
+    case "red":
+        console.log("stop")
+        break
+
+case "yellow":
+    console.log("get ready")
+    break
+
+    case "Green":
+        console.log("Go")
+        break
+
+        default:
+            console.log("Invalid Signal")
+}
