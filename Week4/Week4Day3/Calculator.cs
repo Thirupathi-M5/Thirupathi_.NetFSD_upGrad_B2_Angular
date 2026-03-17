3d128345353a25224693f69e522eebaa6c13ef2a
Kumbar Thirupathi <155380490+Thirupathi-M5@users.noreply.github.com>
2026-03-17 18:02:31 +0530

    Add files via upload

---
+++ Week4/Week4Day3/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+ class Calculator
+{
+    static void Main()
+    {
+        Console.Write("Enter First Number: ");
+        int a= Convert.ToInt32 (Console.ReadLine());
+       
+        Console.Write("Enter Second Numbere: ");
+        int b= Convert.ToInt32 (Console.ReadLine());
+
+        Console.Write("Enter Operator (+, -, *, /): ");
+        char op = Convert.ToChar(Console.ReadLine());
+
+        double result = 0;
+
+        switch (op)
+        {
+            case '+':
+                result = a + b;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '-':
+                result = a - b;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '*':
+                result = a * b;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            case '/':
+                result = a / b;
+                Console.WriteLine("Result: " + result);
+                break;
+
+            Default:
+                Console.WriteLine("Invalid Operator");
+                break;
+        }
+
+
+    }
+}
---
+++ Week4/Week4Day3/EmployeeBonus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class EmployeeBonus
+{
+    public static void Main()
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Enter Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter Experience (years): ");
+        int experience = Convert.ToInt32(Console.ReadLine());
+
+        double BonusPercent;
+         if(experience < 2)
+        {
+            BonusPercent = 0.05;
+        }
+        else if(experience>= 2 && experience<=5) {
+            BonusPercent = 0.10;
+    }
+        else
+        {
+            BonusPercent = 0.15;
+        }
+
+        double Bonus = salary * BonusPercent;
+        double finalsalary = salary + Bonus;
+
+        Console.WriteLine("Employee: " + name);
+        Console.WriteLine("Bonus: " + Bonus);
+        Console.WriteLine("Final Salary: " + finalsalary);
+    }
+
+}
